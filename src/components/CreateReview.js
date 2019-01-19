6d57b3292e4b2051eb3cf1f424dc8127dc0475c3
Susie Son <susieson99@gmail.com>
2019-01-19 13:54:39 -0500

    Added create review

---
+++ src/components/CreateReview.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Rating from './Rating';
+
+export default class FormDialog extends React.Component {
+  state = {
+    open: false
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    return (
+      <div>
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={this.handleClickOpen}
+        >
+          Write a Review
+        </Button>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Rate and Review</DialogTitle>
+          <DialogContent>
+            <Rating />
+            <DialogContentText>
+              Please provide more information about this property here.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="standard-textarea"
+              label="Share details of your experience at this property"
+              placeholder="Placeholder"
+              multiline
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.handleClose} color="primary">
+              Post
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,46 +1,47 @@
 import React from 'react';
-import { compose, withProps, withHandlers } from 'recompose'
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
+import { compose, withProps, withHandlers } from 'recompose';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from 'react-google-maps';
 import MarkerClusterer from 'react-google-maps/lib/components/addons/MarkerClusterer';
 
-const defaultLocation = { lat: 43.659, lng: -79.396 }
-var locations = [
-  {lat: 43.659, lng: -79.396},
-  {lat: 43.669, lng: -79.383},
-  {lat: 43.629, lng: -79.376},
-  {lat: 43.680, lng: -79.346},
-  {lat: 43.519, lng: -79.406},
-  {lat: 43.619, lng: -79.696},
-  {lat: 43.639, lng: -79.354},
-  {lat: 43.699, lng: -79.293}
-]
+const defaultLocation = { lat: 43.659, lng: -79.396 };
+const locations = [
+  { lat: 43.659, lng: -79.396 },
+  { lat: 43.669, lng: -79.383 },
+  { lat: 43.629, lng: -79.376 },
+  { lat: 43.68, lng: -79.346 },
+  { lat: 43.519, lng: -79.406 },
+  { lat: 43.619, lng: -79.696 },
+  { lat: 43.639, lng: -79.354 },
+  { lat: 43.699, lng: -79.293 }
+];
 
 const MapWithClusterer = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyD9HOl0pZippiOooSr3Gbclta9fSJygXc0",
+    googleMapURL:
+      'https://maps.googleapis.com/maps/api/js?key=AIzaSyD9HOl0pZippiOooSr3Gbclta9fSJygXc0',
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />
   }),
   withHandlers({
-    onMarkerClustererClick: () => (markerClusterer) => {
-      const clickedMarkers = markerClusterer.getMarkers()
-      console.log(`Current clicked markers length: ${clickedMarkers.length}`)
-      console.log(clickedMarkers)
+    onMarkerClustererClick: () => markerClusterer => {
+      const clickedMarkers = markerClusterer.getMarkers();
+      console.log(`Current clicked markers length: ${clickedMarkers.length}`);
+      console.log(clickedMarkers);
     }
   }),
   withScriptjs,
   withGoogleMap
-)((props =>
-  <GoogleMap
-    defaultZoom={10}
-    defaultCenter={defaultLocation}>
+)(props => (
+  <GoogleMap defaultZoom={10} defaultCenter={defaultLocation}>
     <MarkerClusterer onClick={props.onMarkerClustererClick}>
       {props.markers.map((marker, index) => (
-        <Marker
-          key={index}
-          position={{ lat: marker.lat, lng: marker.lng }}
-        />
+        <Marker key={index} position={{ lat: marker.lat, lng: marker.lng }} />
       ))}
     </MarkerClusterer>
   </GoogleMap>
@@ -48,9 +49,7 @@ const MapWithClusterer = compose(
 
 class Map extends React.PureComponent {
   render() {
-    return (
-        <MapWithClusterer markers={locations}/>
-    )
+    return <MapWithClusterer markers={locations} />;
   }
 }
 
