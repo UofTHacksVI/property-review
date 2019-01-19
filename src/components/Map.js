b179822cbc8ac1a095a4bba204c3dff7d7bb3112
Susie Son <susieson99@gmail.com>
2019-01-19 06:23:46 -0500

    Added a simple map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps"
 
-const Map = () => <div>Map</div>;
+const SimpleMap = withScriptjs(withGoogleMap(props =>
+  <GoogleMap
+    defaultZoom={10}
+    defaultCenter={{ lat: 43.786, lng: -79.463 }} />
+));
+
+class Map extends React.PureComponent {
+  render() {
+    return (
+        <SimpleMap
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD9HOl0pZippiOooSr3Gbclta9fSJygXc0&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />} />
+    )
+  }
+}
 
 export default Map;
